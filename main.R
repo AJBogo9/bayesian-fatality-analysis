6d69bb27abc8ebe368337b82b1f4e99a8ee16e6f
Andreas Bogossian <andreas.bogossian@aalto.fi>
2025-11-19 14:01:35 +0200

    main.R file not used

--- main.R
+++
@@ -1,7 +0,0 @@
-data <- read.csv("data/aircrashdata.csv")
-names(data)
-
-data_filtered <- data[data$Year > 1970, c("Year", "Aircraft.Manufacturer", "Aircraft",
-                                 "Location", "Operator", "Ground", "Fatalities..air.",
-                                 "Aboard")]
-dim(data_filtered)
